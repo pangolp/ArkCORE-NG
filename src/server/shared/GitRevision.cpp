327993e65c74823f416a8752ba25d0b4aee44969
Pagani Walter <paganiwalter@gmail.com>
2019-12-08 21:59:11 -0300

    Remove _BUILD_DIRECTIVE from GitRevision.cpp

--- src/server/shared/GitRevision.cpp
+++ src/server/shared/GitRevision.cpp
@@ -87,7 +87,7 @@ char const* GitRevision::GetFullDatabase()
 char const* GitRevision::GetFullVersion()
 {
   return "ArkCore4-NG rev. " VER_PRODUCTVERSION_STR
-    " (" TRINITY_PLATFORM_STR ", " _BUILD_DIRECTIVE ", " TRINITY_LINKAGE_TYPE_STR ")";
+    " (" TRINITY_PLATFORM_STR ", " ", " TRINITY_LINKAGE_TYPE_STR ")";
 }
 
 char const* GitRevision::GetCompanyNameStr()
